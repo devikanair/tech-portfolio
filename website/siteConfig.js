5e73cdf4071e8f5bf9991afb43f0c27e49b22dd1
Devika <s.devikanair@gmail.com>
2020-10-02 12:38:37 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -54,7 +54,7 @@ const siteConfig = {
         // Highlight.js theme to use for syntax highlighting in code blocks.
         theme: 'default',
     },
-
+ 
     // Add custom scripts here that would be placed in <script> tags.
     scripts: ['https://buttons.github.io/buttons.js'],
 
